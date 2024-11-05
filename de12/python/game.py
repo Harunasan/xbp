f8ca37a178daafa083dbe763880dbae6992621d0
Harunasan <r202402067za@jindai.jp>
2024-11-05 22:32:49 +0900

    1105

---
+++ de12/python/game.py
@@ -0,0 +1,9 @@
+from os import name
+from re import A
+from select import select
+ok = 0
+name=input("あなたのお名前は？")
+def intro():
+    print("あなたの友達がゾンビになってしまいました！")
+    print("あなたはどうする？？？以下の質問に答えてみよう👻")
+    print
